aabeb5b71a490161a358ff91ecce821a357d99d1
artamonovoleg <ArtamonovAV2003@gmail.com>
2021-04-07 23:30:50 +0300

    Mandelbrot set

--- shaders/mandelbrot.glsl
+++ shaders/mandelbrot.glsl
@@ -1,25 +1,17 @@
 #version 330 core
-#extension GL_ARB_separate_shader_objects : enable
-
-layout(location = 0) in vec2 uv;
 
 uniform vec2 u_resolution;
 
 out vec4 fragColor;
 
-int maxIterations = 50;
+int maxIterations = 90;
 
-// vec2 colors [2] {
-// 	vec3(0.2, 0.3, 0.4),
-// 	vec3(0.2, 0.3, 0.5)
-// }
-
-float map(float x, float in_min, float in_max, float out_min, float out_max)
+float Map(float x, float in_min, float in_max, float out_min, float out_max)
 {
 	return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
-vec2 squareImaginary(vec2 number)
+vec2 SquareImaginary(vec2 number)
 {
 	return vec2(
 		pow(number.x, 2) - pow(number.y, 2),
@@ -33,7 +25,7 @@ vec3 IterateMandelbrot(vec2 coord)
 
 	for(int i = 0; i < maxIterations; i++)
 	{
-		z = squareImaginary(z) + coord;
+		z = SquareImaginary(z) + coord;
 		if (length(z) > 2) 
 			return vec3(0.0, float(i) / maxIterations, float(i) / maxIterations);
 	}
@@ -44,7 +36,7 @@ vec3 IterateMandelbrot(vec2 coord)
 void main()
 {
 	vec2 coord = gl_FragCoord.xy / u_resolution - vec2(1.0);
-	coord = vec2(map(coord.x, -1.0, 1.0, -2.0, 1.0), coord.y);
+	coord = vec2(Map(coord.x, -1.0, 1.0, -2.0, 1.0), coord.y);
 
 	vec3 c = IterateMandelbrot(coord);
     fragColor = vec4(c, 1.0f);
--- shaders/vert.glsl
+++ shaders/vert.glsl
@@ -1,7 +1,4 @@
 #version 330 core
-#extension GL_ARB_separate_shader_objects : enable
-
-layout(location = 0) out vec2 uv;
 
 void main() 
 {
@@ -9,5 +6,4 @@ void main()
     float y = float(((uint(gl_VertexID) + 1u) / 3u)%2u); 
 
     gl_Position = vec4(-1.0f + x*2.0f, -1.0f+y*2.0f, 0.0f, 1.0f);
-    uv = vec2(x, y);
 }
